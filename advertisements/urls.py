517c39af24120352163b60e9e194cdd615f6a787
YOUR_NAME <YOUR_EMAIL>
2022-10-24 21:38:56 +0300

    first commit from git app

--- advertisements/urls.py
+++ advertisements/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.Advertisement.as_view(), name='advertisement_list'),
     path('advertisement', views.advertisement_prices, name='advertisement_prices'),
-    path('about', views.About.as_view(template_name='advertisements/about.html'), name='about')
+    path('contacts', views.Contacts.as_view(template_name='advertisements/about.html'), name='contacts'),
+    path('about', views.About.as_view(template_name='advertisements/about.html'), name='about'),
 ]
--- advertisements/views.py
+++ advertisements/views.py
@@ -1,3 +1,4 @@
+from django.contrib.sessions.models import Session
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
@@ -25,19 +26,26 @@ class Advertisement(View):
             request.session['hit'] = 0
         else:
             request.session['hit'] += 1
-        print(request.session.items())
         return render(request, 'advertisements/advertisement_list.html', {'advertisements': self.advertisements,
                                                                           'answer': self.answer + str(hit)})
+
+
+class Contacts(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'Contacts our organization!'
+        context['title'] = 'Contacts'
+        context['description'] = '\nAddress: ...' \
+                                 '\nPhone: ...' \
+                                 '\nTelegram: ...' \
+                                 '\nInstagram: ... '
+        return context
 
 
 class About(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['name'] = 'About our organization!'
-        context['title'] = 'Information'
-        context['description'] = 'Something information about our organization.' \
-                                 '\nAddress: ...' \
-                                 '\nPhone: ...' \
-                                 '\nTelegram: ...' \
-                                 '\nInstagram: ... '
+        context['name'] = 'Information about us!'
+        context['title'] = 'About'
+        context['description'] = 'We are small bakery with delicious cupcakes, cakes, macaroons etc.'
         return context
